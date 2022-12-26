f7757993f96a6fb929851b79a5844a5a9268e471
d1av <davi4alves@gmail.com>
2022-12-25 21:32:42 -0300

    chore: initial structure

---
+++ src/app/shared/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarTopComponent } from './layout/navbar-top/navbar-top.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarTopComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
